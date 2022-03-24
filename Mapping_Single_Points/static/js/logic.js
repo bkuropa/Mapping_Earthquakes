b975a934be409adcfeefd6ffd0d3f11cc886c7cd
Bryan Kuropatwa <bkuropa@gmail.com>
2022-03-24 11:30:46 -0400

    New branch

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -4,11 +4,20 @@ console.log("Working");
 // Create the map object with a centre [,] and zoom ,4 level:
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+// Add a marker for Los Angeles, California
+/*circle1 = L.circle([34.0522, -118.2437], 300).addTo(map);
+circle1.setStyle({color: 'black', fillColor: 'yellow'}); */
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
+
 // The tile layer is the background of the map:
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    //id: 'mapbox/streets-v11',
+    maxZoom: 20,
+    id: 'mapbox/dark-v10',
     //tileSize: 512,
     //zoomOffset: -1,
     accessToken: API_KEY
